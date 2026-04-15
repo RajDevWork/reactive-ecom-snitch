90a4470b249242d05be1feeee48455b58cff3fd8
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-04-15 10:46:00 +0530

    centralize env variable and connected to DB

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
-import dotenv from 'dotenv'
 import app from './src/app.js'
-
-dotenv.config()
+import connectDB from './src/config/db.js'
 const PORT = process.env.PORT || 3000
+
 
 //listen and start server
 
 const startServer = async ()=>{
-
     try{
-
+        await connectDB()
         app.listen(PORT, ()=>{
             console.log(`Server is running on PORT ${PORT}`)
         })
